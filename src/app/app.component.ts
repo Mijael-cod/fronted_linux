fe73bf39e5d93e99e4a718f3005dd1d4f18839ef
Mijael Aliaga <136122462+TecnologiaGett@users.noreply.github.com>
2023-10-31 17:16:58 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './service/api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  data: any[] = [];
+
+constructor(private apiService: ApiService){
+
+}
+
+
+  llenarData(){
+    this.apiService.getData().subscribe(data => {
+      this.data = data;
+      console.log(this.data);
+    })
+  }
+
+
+
+
+  ngOnInit(): void {
+    this.llenarData()
+  }
+
   title = 'front_angular';
 }
